a07b6ad107d762e3c04e738a46f85fae87a57636
Jerpancho <panchojerrold@gmail.com>
2022-08-10 14:41:02 -0500

    added some styling

--- valoguesser/src/App.js
+++ valoguesser/src/App.js
@@ -1,9 +1,20 @@
+import "./app.css";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Default
+    <div className="app">
+      <header className="app-header">
+        <h1>Valorant Guesser</h1>
       </header>
+      <main>
+        {/* this section will be replaced by images of instructions? */}
+        <div className="tutorial">
+          <section>test</section>
+          <section>test</section>
+          <section>test</section>
+          <section>test</section>
+        </div>
+      </main>
     </div>
   );
 }
