9580d54fc6a8f4fe329fb59e31988d2bac757641
Jerpancho <panchojerrold@gmail.com>
2022-08-12 11:02:59 -0500

    added map selection buttons

--- valoguesser/src/components/select.js
+++ valoguesser/src/components/select.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Select = () => {
+  // this list should be taken from an api with proper unique ids
+  const [mapList, setMapList] = useState(["Ascent", "Bind", "Haven"]);
+
+  const [selected, setSelected] = useState("");
+  console.log(selected);
   return (
     <div className="app">
       <header className="app-header">
         <h1> Map Selection</h1>
       </header>
+      <main>
+        <ul className="map-selection">
+          {mapList.map((item, index) => (
+            <li key={index}>
+              <input
+                type="button"
+                value={item}
+                name={item}
+                onClick={(e) => setSelected(e.target.name)}
+              />
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 };
